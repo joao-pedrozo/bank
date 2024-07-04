b5dfbc0c5f5b2124c6a2279aa8bc97d8cfdb852c
P <jp.professional@outlook.com>
2024-07-04 12:13:57 -0300

    feat: setup base server

---
+++ apps/api/server.ts
@@ -0,0 +1,53 @@
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import {
+  getGraphQLParameters,
+  processRequest,
+  renderGraphiQL,
+  sendResult,
+  shouldRenderGraphiQL,
+} from "graphql-helix";
+// import { schema } from "./schema";
+
+// declare module "koa" {
+//   interface Request {
+//     body?: any;
+//     rawBody: string;
+//   }
+// }
+
+const app = new Koa();
+
+app.use(bodyParser());
+
+app.use(async (ctx) => {
+  const request = {
+    body: ctx.request.body,
+    headers: ctx.req.headers,
+    method: ctx.request.method,
+    query: ctx.request.query,
+  };
+
+  if (shouldRenderGraphiQL(request)) {
+    ctx.body = renderGraphiQL({});
+  } else {
+    const { operationName, query, variables } = getGraphQLParameters(request);
+
+    const result = await processRequest({
+      operationName,
+      query,
+      variables,
+      request,
+      // schema,
+    });
+
+    ctx.respond = false;
+    sendResult(result, ctx.res);
+  }
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`GraphQL server is running on port ${port}.`);
+});
