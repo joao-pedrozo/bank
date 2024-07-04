2edee3f355d9167c79706a4f89a2534023e511f0
P <jp.professional@outlook.com>
2024-07-04 11:45:53 -0300

    fix: simple build errors

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import Home from "@/components/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return <Home />;
 }
 
